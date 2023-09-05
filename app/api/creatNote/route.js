34c62eea61dd155c53c4bc4a27e167e8503d0739
Elisha2605 <elisha_ngoma@yahoo.fr>
2023-09-05 17:21:08 +0200

    fixed MongoDB issue

--- app/api/creatNote/route.js
+++ app/api/creatNote/route.js
@@ -5,48 +5,40 @@ import mongoose from "mongoose"
 import { NextResponse } from "next/server"
 
 export async function POST(req){
-            //don't know if i should extract the userId from the request.???
-  const {newNoteTitle, newNote, category, userId} = await req.json()
-  
   try {
-      // const { userId } = req.session; // getting user Session from the request session __posted from the front-end
-      // const userId = "user_2UcCfLTdGCXcDa5lmUFiTQAwk8x";
-      await mongodb()
-      const createdNote = await newNotes.create({
-        userId,                     // Creating the document using the userId from the req.session 
-        newNoteTitle,
-        newNote,
-        category
-      });
+    const userId = "user_2UcCfLTdGCXcDa5lmUFiTQAwk8x"; // I used this as a temp userId, you may consider changing it
+    await mongodb();
 
-      console.log('New note has been created!');
-      console.log(createdNote);
+    const requestData = await req.json();
 
-      return NextResponse.json({
-          results: ['Data sent succefully'],
-          success: true,
-      })
-      
+    const { newNoteTitle, newNote, category } = requestData;
+
+    const createdNote = new newNotes({
+      userId,
+      newNoteTitle,
+      newNote,
+      category,
+    });
+  
+    await createdNote.save();
+
+    console.log('New note has been created!');
+    console.log(createdNote);
+
+    return NextResponse.json({
+      results: ['Data sent successfully'],
+      success: true,
+    });
   } catch (error) {
-    
-    if(error instanceof mongoose.Error.ValidationError){
-      let errorList = []
-      for (let e in error.errors){
-        errorList.push(error.errors[e].message)
+    if (error instanceof mongoose.Error.ValidationError) {
+      let errorList = [];
+      for (let e in error.errors) {
+        errorList.push(error.errors[e].message);
       }
-      
-      console.log(errorList)
-      return NextResponse.json({results: errorList})
-    }else{
-      return NextResponse.json({results: ['Unable to Create A Note']})
+      console.log(errorList);
+      return NextResponse.json({ results: errorList });
+    } else {
+      return NextResponse.json({ results: ['Unable to Create A Note'] });
     }
-    console.log(error)
-       
   }
-    
 }
-// console.error("An error occurred:", error);
-//     return NextResponse.json({
-//       results: ['An error occurred while processing your request'],
-//       success: false,
-//     })  
