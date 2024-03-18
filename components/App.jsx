9fb519c2403f44703ae7b4ff94f6e854bbd4184f
Herman Athor <hermanathorg@outlook.com>
2024-03-18 11:52:46 +0100

    Started to add a proper landing page

--- components/App.jsx
+++ components/App.jsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import LandingPage from "./LandingPage";
+import HeroSection from "./landing-page/HeroSection";
 //import { UserButton, currentUser } from "@clerk/nextjs";
 
 async function App() {
@@ -16,62 +18,60 @@ async function App() {
   const currentDate = date.toLocaleString("en-DK", options);
   return (
     <>
-      <section className="bg-white dark:bg-gray-900 bg-[url('/images/notes-photo.jpg')] bg-no-repeat bg-fixed bg-cover h-screen w-full">
-        <div>
-          {user && (
-            <div className="flex justify-center  pt-5 px-5">
-              <div className="flex flex-row gap-2 justify-between items-center w-full text-2xl ">
-                <p className="text-sm md:text-xl">{currentDate}</p>
-                <div className="flex flex-row justify-center items-center gap-2">
-                  profile
-                  {/* <p className="">{user.firstName}</p>
-                  <div>
-                    <UserButton afterSignOutUrl="/" />
-                  </div> */}
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-          <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-              Notes Solution for Productivity
-            </h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-3xl dark:text-gray-400">
-              Targeted to keep you in sync and improve your productivity. From
-              all the tiny notes details to your reminders, financial, todos and
-              Work.
-            </p>
-            <Link
-              href="/recipes"
-              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
-            >
-              View Notes
-              <svg
-                className="w-5 h-5 ml-2 -mr-1"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </Link>
-            <Link
-              href="/createNote"
-              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-            >
-              Start Creating Notes
-            </Link>
-          </div>
-        </div>
-      </section>
+      <HeroSection />
+      <HeroSection />
     </>
   );
 }
 
 export default App;
+
+{
+  /* <section className="bg-white dark:bg-gray-900 bg-[url('/images/notes-photo.jpg')] bg-no-repeat bg-fixed bg-cover h-screen w-full">
+  <div>
+    {user && (
+      <div className="flex justify-center  pt-5 px-5">
+        <div className="flex flex-row gap-2 justify-between items-center w-full text-2xl ">
+          <p className="text-sm md:text-xl">{currentDate}</p>
+          
+        </div>
+      </div>
+    )}
+  </div>
+  <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+    <div className="mr-auto place-self-center lg:col-span-7">
+      <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+        Notes Solution for Productivity
+      </h1>
+      <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-3xl dark:text-gray-400">
+        Targeted to keep you in sync and improve your productivity. From all the
+        tiny notes details to your reminders, financial, todos and Work.
+      </p>
+      <Link
+        href="/recipes"
+        className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+      >
+        View Notes
+        <svg
+          className="w-5 h-5 ml-2 -mr-1"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+      </Link>
+      <Link
+        href="/createNote"
+        className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+      >
+        Start Creating Notes
+      </Link>
+    </div>
+  </div>
+</section>; */
+}
